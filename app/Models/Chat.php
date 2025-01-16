99ed4c50ba03bde424b8144fa4378ff4350c1c47
jthamayo <jtamayobalogh@gmail.com>
2025-01-16 14:08:55 +0100

    feat: Add table models

---
+++ app/Models/Chat.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Notifications\Notifiable;
+
+class Chat extends Model implements HasMedia
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+    use InteractsWithMedia;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'type',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'creation_date' => 'datetime',
+        ];
+    }
+}
