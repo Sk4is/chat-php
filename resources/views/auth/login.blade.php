09b633d1b8cfa8a2d21f77cafead6557b221dd14
Sk4is-1 <adrianperezagredano@gmail.com>
2025-02-10 14:40:53 +0100

    register and login views changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,8 @@
         <!-- Email Address -->
         <div>
             <label for="email" :value="__('Email')" class="text-blue-1">Email</label>
-            <input id="email" class="block mt-1 w-full bg-light border-2 border-primary rounded-md p-2" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <input id="email" class="block mt-1 w-full bg-light border-2 border-primary rounded-md p-2 text-blue-10" type="email"
+                name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -16,10 +17,8 @@
         <div class="mt-4">
             <label for="password" :value="__('Password')" class="text-blue-1">Password</label>
 
-            <input id="password" class="block mt-1 w-full bg-light border-2 border-primary rounded-md p-2"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <input id="password" class="block mt-1 w-full bg-light border-2 border-primary rounded-md p-2 text-blue-10"
+                type="password" name="password" required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -27,19 +26,23 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                    name="remember">
                 <span class="ms-2 text-sm text-blue-1">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-blue-1 hover:text-blue-7 rounded-md" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-blue-1 hover:text-blue-3 rounded-md"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <button class="ms-3 bg-blue-1 text-white py-2 px-4 rounded-lg hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-primary-dark">
+            <button
+                class="ms-3 bg-blue-1 text-white py-2 px-4 rounded-lg hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-primary-dark">
                 {{ __('Log in') }}
             </button>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
         <!-- Name -->
         <div>
             <label for="name" class="text-blue-1">Name</label>
-            <input id="name" 
-                          class="block mt-1 w-full border-2 rounded-md p-2" 
-                          type="text" 
-                          name="name" 
-                          :value="old('name')" 
+            <input id="name"
+                          class="block mt-1 w-full border-2 rounded-md p-2 text-blue-10"
+                          type="text"
+                          name="name"
+                          :value="old('name')"
                           required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -17,11 +17,11 @@
         <!-- Email Address -->
         <div class="mt-4">
             <label for="email" :value="__('Email')" class="text-blue-1">Email</ label>
-            <input id="email" 
-                          class="block mt-1 w-full border-2 rounded-md p-2" 
-                          type="email" 
-                          name="email" 
-                          :value="old('email')" 
+            <input id="email"
+                          class="block mt-1 w-full border-2 rounded-md p-2 text-blue-10"
+                          type="email"
+                          name="email"
+                          :value="old('email')"
                           required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
@@ -29,10 +29,10 @@
         <!-- Password -->
         <div class="mt-4">
             <label for="password" :value="__('Password')" class="text-blue-1">Password</label>
-            <input id="password" 
-                          class="block mt-1 w-full border-2 rounded-md p-2" 
-                          type="password" 
-                          name="password" 
+            <input id="password"
+                          class="block mt-1 w-full border-2 rounded-md p-2 text-blue-10"
+                          type="password"
+                          name="password"
                           required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -40,16 +40,16 @@
         <!-- Confirm Password -->
         <div class="mt-4">
             <label for="password_confirmation" :value="__('Confirm Password')" class="text-blue-1">Confirm Password</label>
-            <input id="password_confirmation" 
-                          class="block mb-6 mt-1 w-full border-2 rounded-md p-2" 
-                          type="password" 
-                          name="password_confirmation" 
+            <input id="password_confirmation"
+                          class="block mb-6 mt-1 w-full border-2 rounded-md p-2 text-blue-10"
+                          type="password"
+                          name="password_confirmation"
                           required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-blue-1 hover:text-blue-7 rounded-md" href="{{ route('login') }}">
+            <a class="underline text-sm text-blue-1 hover:text-blue-3 rounded-md" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
