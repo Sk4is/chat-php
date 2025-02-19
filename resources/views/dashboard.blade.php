461c361b44c126d4718c8b8ca268a4a20a7114a9
jthamayo <jtamayobalogh@gmail.com>
2025-02-19 14:00:41 +0100

    chore: change header text to blue

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,12 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex items-center justify-between">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-blue-4 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        <button class="bg-blue-4 text-white px-4 py-2 rounded ">Add chat</button>
+        <button class="bg-blue-4 text-white px-4 py-2 rounded ">New chat</button>
         </div>
     </x-slot>
-
-   
 
     <div class="w-screen flex h-[600px]">
     <div class="available-chats w-1/3 h-full bg-blue-8">
@@ -34,8 +32,8 @@
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-blue-1 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 text-blue-8 dark:text-gray-100">
-                            {{ __("You're logged in!") }}
+                        <div class="p-6 text-blue-8">
+                            {{ __("You were missed! - You have (1000) new messages") }}
                         </div>
                     </div>
                 </div>
